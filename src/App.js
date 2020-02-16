8592caf39f3f0f8a82e42500746c8e6c3d6262bf
Yamid <yamidcuetomazo@gmail.com>
2020-02-16 17:20:26 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* esversion:6 */
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React Fundamentos
         </a>
       </header>
     </div>
