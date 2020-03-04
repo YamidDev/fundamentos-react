7a9d1a46f78feab48104a992fafe7bb96b6c1793
Yamid <yamidcuetomazo@gmail.com>
2020-03-03 22:23:43 -0500

    agregando iconos de material y aprendiendo a usar los class components

--- src/index.js
+++ src/index.js
@@ -11,13 +11,50 @@ function getInfo(user){
     return `Hola soy ${user1.name} y tengo ${user1.age} años y el doble de mi edad ${user1.age*2}`
 }*/
 
-const TarjetaFruta = (props) => (
+/*const TarjetaFruta = (props) => (
     <div className={'container'}>
         <h3>{props.name}</h3>
         <hr/>
         <p>Precio ${props.price}</p>
     </div>
-)
+)*/
+class TarjetaFruta extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            cantidad: 0
+        }
+    }
+
+    render() {
+        return (
+            <div className={'container my-5'}>
+                <h3>{this.props.name}</h3>
+                <div>Cantidad: {this.state.cantidad}</div>
+                <button type={'button'} className={'btn btn-primary'}
+                        onClick={()=>{
+                            this.setState({
+                                cantidad: this.state.cantidad + 1
+                            })
+                        }} style={{marginRight: '5px'}}
+                >
+                    <b><i className={'material-icons'}>add_shopping_cart</i></b></button>
+                <button type={'button'} className={'btn btn-danger'}
+                        onClick={()=>{
+                            this.setState({
+                                cantidad: this.state.cantidad - 1
+                            })
+                        }}
+                >
+                    <b><i className={'material-icons'}>remove_shopping_cart</i></b>
+                </button>
+                <hr/>
+                <p>${this.props.price}</p>
+            </div>
+        )
+    }
+}
+
 
 const App = () => (
     <div>
